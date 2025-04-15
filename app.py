70b3365dea56bb8a33a6c23c4c21b0ebb679d84c
yogibaba7 <yogeshchouhan263@gmail.com>
2025-04-15 17:14:02 +0530

    improve code quality

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-def add(a,b):return a+b
+def add(a, b): return a+b
 
-def sub(a,b):return a-b
-result1 = add(2,3)
-result2 =  sub(3,2)
+
+def sub(a, b): return a-b
+
+
+result1 = add(2, 3)
+result2 = sub(3, 2)
 
 print(result1)
 print(result2)
