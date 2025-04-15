1e1fd15d899bda9371afeaed90c70efa356f793d
yogibaba7 <yogeshchouhan263@gmail.com>
2025-04-15 17:50:09 +0530

    check workflow on pull_request

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ def add(a, b): return a+b
 def sub(a, b): return a-b
 
 
+def mul(a, b): return a*b
+
+
 result1 = add(2, 3)
 result2 = sub(3, 2)
+result3 = mul(4,3)
 
 print(result1)
 print(result2)
+print(result3)
